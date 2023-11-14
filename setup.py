eaaf6cc8e3a350378508c1e42dd936eb57955776
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2023-11-14 17:51:51 +0100

    [FIX] handling cysetuptools
    Added requirement for setuptools for handling pyproject.toml

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # Authors : Andreas Schälicke <andreas.schaelike@helmholtz-berlin.de>
 #           Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
-# Date : 2017, 2020
-
+# Date : 2017, 2020, 2023
+import sys, os.path
+# required that cysetuptools is found
+sys.path.append(os.path.dirname(__file__))
 from cysetuptools import setup
 import protocol_buffer
 
