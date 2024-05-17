e6395c597e552799f0336a584443f99a55223939
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2024-05-17 12:16:11 +0200

    [TASK] build proto buffer files when building
    For automatic builds

--- setup.py
+++ setup.py
@@ -1,14 +1,41 @@
 # -*- coding: utf-8 -*-
 # Authors : Andreas Schälicke <andreas.schaelike@helmholtz-berlin.de>
 #           Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
-# Date : 2017, 2020, 2023
-import sys, os.path
+# Date : 2017, 2020, 2023, 2024
+
+import sys
+import os.path
+
 # required that cysetuptools is found
 sys.path.append(os.path.dirname(__file__))
+
 from cysetuptools import setup
+from setuptools.command.build import build
+from setuptools import distutils
 import protocol_buffer
 
-cmdclass = dict(build_proto_c=protocol_buffer.GenerateProtocolBuffer)
+
+class ActionOnBuild(build):
+    def __init__(self, dist, **kwargs):
+        build.__init__(self, dist, **kwargs)
+        # just default args
+        self._build_proto_c = protocol_buffer.GenerateProtocolBuffer(dist)
+
+    def run(self):
+        self.announce(
+            "Building archiver: creating protocol buffer", level=distutils.log.INFO
+        )
+        self._build_proto_c.run()
+        self.announce(
+            "Building archiver: running standard build", level=distutils.log.INFO
+        )
+        build.run(self)
+
+
+cmdclass = dict(
+    build_proto_c=protocol_buffer.GenerateProtocolBuffer, build=ActionOnBuild
+)
+
 setup(
     cmdclass=cmdclass,
     author="Andreas Schälicke, Pierre Schnizer",
