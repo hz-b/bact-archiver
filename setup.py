7e582fd240534cf441294ddd6e0d976196b2ee8f
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2024-05-22 16:38:45 +0200

    [TASK] setup.cfg info in pyproject

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@
 
 from setuptools import Extension, setup
 from setuptools_protobuf import Protobuf
+from numpy import get_include
 
 setup(
     author="Andreas Schälicke, Pierre Schnizer",
@@ -12,12 +13,12 @@ setup(
         "andreas.schälicke@helmholtz-berlin.de",
         "pierre.schnizer@helmholtz-berlin.de"
     ),
-    protobufs = [Protobuf("proto/epics_event.proto")],
+    protobufs=[Protobuf("proto/epics_event.proto")],
     ext_modules=[Extension(
         name="bact_archiver.epics_event",
         sources=["proto/epics_event.pyx", "proto/epics_event.pb.cc"],
-        include_dirs=["."],
-        libraries=["protobuf"],
+        include_dirs=[".", get_include()],
+        libraries=["protobuf", ],
         language="c++"
     )],
 )
