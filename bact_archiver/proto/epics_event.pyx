20346f8aaec96893b7aada2de365f4d088dcb266
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2024-05-24 17:59:28 +0200

    [Snapshot] towards stuptools_protobuf

--- bact_archiver/proto/epics_event.pyx
+++ bact_archiver/proto/epics_event.pyx
@@ -17,10 +17,10 @@ The following functions are expected to be called by external modules?
 """
 
 # read EPICSEvent.pxd definition of Protocol-Buffer code
-from proto.epics_event cimport PayloadInfo,  string
-from proto.epics_event cimport ScalarDouble, ScalarString, ScalarEnum, ScalarInt
-from proto.epics_event cimport VectorDouble, VectorFloat
-from proto.epics_event cimport VectorInt, VectorShort, VectorChar
+from bact_archiver.proto.epics_event cimport PayloadInfo,  string
+from bact_archiver.proto.epics_event cimport ScalarDouble, ScalarString, ScalarEnum, ScalarInt
+from bact_archiver.proto.epics_event cimport VectorDouble, VectorFloat
+from bact_archiver.proto.epics_event cimport VectorInt, VectorShort, VectorChar
 
 import numpy as np
 cimport numpy as np
--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@
 # Date : 2017, 2020, 2023, 2024
 
 from setuptools import Extension, setup
-from setuptools_protobuf import Protobuf
 from numpy import get_include
 
 setup(
-    author="Andreas Schälicke, Pierre Schnizer",
-    author_email=(
-        "andreas.schälicke@helmholtz-berlin.de",
-        "pierre.schnizer@helmholtz-berlin.de"
-    ),
-    protobufs=[Protobuf("proto/epics_event.proto")],
-    ext_modules=[Extension(
-        name="bact_archiver.epics_event",
-        sources=["proto/epics_event.pyx", "proto/epics_event.pb.cc"],
-        include_dirs=[".", get_include()],
-        libraries=["protobuf", ],
-        language="c++"
-    )],
+    ext_modules=[
+        Extension(
+            name="bact_archiver.epics_event",
+            sources=[
+                "bact_archiver/proto/epics_event.pyx",
+                "bact_archiver/proto/epics_event.pb.cc",
+            ],
+            include_dirs=[".", get_include()],
+            libraries=[
+                "protobuf",
+            ],
+            language="c++",
+        )
+    ],
 )
